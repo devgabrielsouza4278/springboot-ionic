f193f98f82dc3a25c7eb3526b8ef34c7c73f45d3
Gabriel Souza <gabriel_alves.14@hotmail.com>
2021-06-18 22:09:41 -0300

    Validação de e-mail no PUT de Clientes

--- src/main/java/com/gabrielSouza/cursomc/dto/ClienteDTO.java
+++ src/main/java/com/gabrielSouza/cursomc/dto/ClienteDTO.java
@@ -8,7 +8,9 @@ import javax.validation.constraints.NotEmpty;
 import org.hibernate.validator.constraints.Length;
 
 import com.gabrielSouza.cursomc.domain.Cliente;
+import com.gabrielSouza.cursomc.service.validation.ClienteUpdate;
 
+@ClienteUpdate
 public class ClienteDTO implements Serializable {
 
 	private static final long serialVersionUID = 1L;
