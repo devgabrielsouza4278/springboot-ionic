8d7f83f4e8b69589703558cbd86a1619b9be0dc4
Gabriel Souza <gabriel_alves.14@hotmail.com>
2021-06-18 21:34:03 -0300

    Validação email repetido

--- src/main/java/com/gabrielSouza/cursomc/service/validation/ClienteInsertValidator.java
+++ src/main/java/com/gabrielSouza/cursomc/service/validation/ClienteInsertValidator.java
@@ -6,12 +6,21 @@ import java.util.List;
 import javax.validation.ConstraintValidator;
 import javax.validation.ConstraintValidatorContext;
 
+import org.springframework.beans.factory.annotation.Autowired;
+
+import com.gabrielSouza.cursomc.domain.Cliente;
 import com.gabrielSouza.cursomc.domain.Enums.TipoCliente;
 import com.gabrielSouza.cursomc.dto.ClienteNewDTO;
+import com.gabrielSouza.cursomc.repositories.ClienteRepository;
 import com.gabrielSouza.cursomc.service.exception.FieldException;
 import com.gabrielSouza.cursomc.service.validation.utils.BR;
 
 public class ClienteInsertValidator implements ConstraintValidator<ClienteInsert, ClienteNewDTO> {
+	
+	@Autowired
+	private ClienteRepository repo;
+	
+	
 	@Override
 	public void initialize(ClienteInsert ann) {
 	}
@@ -19,13 +28,18 @@ public class ClienteInsertValidator implements ConstraintValidator<ClienteInsert
 	@Override
 	public boolean isValid(ClienteNewDTO objDto, ConstraintValidatorContext context) {
 		List<FieldException> list = new ArrayList<>();
-		
+
 		if (objDto.getTipo().equals(TipoCliente.PESSOAFISICA.getCod()) && !BR.isValidCPF(objDto.getCpfOuCnpj())) {
 			list.add(new FieldException("CpfOuCnpj", "CPF Inválido"));
 		}
-		
+
 		if (objDto.getTipo().equals(TipoCliente.PESSOAJURIDICA.getCod()) && !BR.isValidCNPJ(objDto.getCpfOuCnpj())) {
 			list.add(new FieldException("CpfOuCnpj", "CNPJ Inválido"));
+		}
+		
+		Cliente aux = repo.findByEmail(objDto.getEmail());
+		if(aux !=null) {
+			list.add(new FieldException("email", "O Email já está cadastrado"));
 		}
 		
 		for (FieldException e : list) {
